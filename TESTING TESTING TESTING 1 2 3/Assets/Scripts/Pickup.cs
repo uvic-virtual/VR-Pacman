33966403548f351821d404b0d5b2353819266691
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2019-11-16 12:45:13 -0800

    Created scene to test pickups and score; added ball that dissapears on pickup

---
+++ TESTING TESTING TESTING 1 2 3/Assets/Scripts/Pickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("PickupOrb")){
+			other.gameObject.SetActive (false);
+		}
+	}
+}
