d16fc1465fe1910ddaf3aaffe4fbd35b931464ae
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2019-11-16 13:46:53 -0800

    Spheres give points displayed on screen

--- TESTING TESTING TESTING 1 2 3/Assets/Scripts/Pickup.cs
+++ TESTING TESTING TESTING 1 2 3/Assets/Scripts/Pickup.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Pickup : MonoBehaviour {
+
+	private int count;
+	public Text countText;
+	void Start (){
+		count = 0;
+		SetCountText();
+	}
 	void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.CompareTag("PickupOrb")){
 			other.gameObject.SetActive (false);
+			count = count + 10;
+			SetCountText();
 		}
+	}
+	void SetCountText (){
+		countText.text = "Count: " + count.ToString ();
 	}
 }
