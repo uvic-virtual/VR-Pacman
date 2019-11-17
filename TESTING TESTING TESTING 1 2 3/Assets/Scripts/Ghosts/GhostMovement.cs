eb5532bf85d946e61e8368873a1ac4bfe090e2e6
deanroland <45372296+deanroland@users.noreply.github.com>
2019-11-16 22:28:52 -0800

    Created base class for ghost movement behaviors

---
+++ TESTING TESTING TESTING 1 2 3/Assets/Scripts/Ghosts/GhostMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class GhostMovement : MonoBehaviour
+{
+    [SerializeField] private float DestinationDistanceCheck = 0.5f;
+
+    [SerializeField] private List<Transform> ScatterPoints;
+
+    public enum State { Chase, Frightened, Scatter };
+
+    public static State CurrentState;
+
+    private static GameObject Player;
+
+    private NavMeshAgent Agent;
+
+    private void Start()
+    {
+        if (Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        bool AtDestination = Vector3.Distance(transform.position, Agent.destination) < DestinationDistanceCheck;
+        
+        if (AtDestination)
+        {
+            Agent.SetDestination(NextDestination());
+        }
+    }
+
+    protected abstract Vector3 NextDestination();
+}
