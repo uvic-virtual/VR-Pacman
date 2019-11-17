42c5dd2f2769f65341f9192f75355e2cef966b41
deanroland <45372296+deanroland@users.noreply.github.com>
2019-11-16 22:32:05 -0800

    created stub blinky behavior

---
+++ TESTING TESTING TESTING 1 2 3/Assets/Scripts/Ghosts/Blinky.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Implements Blinky (Red)'s movement behavior.
+/// He agressively chases after the player by targeting the waypoint in front of the player.</summary>
+public class Blinky : GhostMovement
+{
+    protected override Vector3 NextDestination()
+    {
+        if (CurrentState == State.Chase)
+        {
+            return Vector3.zero;
+        }
+        else if (CurrentState == State.Frightened)
+        {
+            return Vector3.zero;
+        }
+        return Vector3.zero;
+    }
+}
