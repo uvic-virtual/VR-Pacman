fbf3e905db87ca83ae7daf8b20b4169fbf5eec1c
Domenic Woo <37358453+icecreamin@users.noreply.github.com>
2019-11-25 21:18:35 -0800

    Changed minimap camera and added a player cursor

---
+++ VR Pacman 2019/Assets/Scripts/MapCursorFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCursorFollow : MonoBehaviour {
+
+	public Transform obj1;
+	public Transform obj2;
+
+
+	// Update is called once per frame
+	void Update () {
+		obj2.rotation = Quaternion.Euler(-90, obj1.eulerAngles.y, 180);
+	}
+}
