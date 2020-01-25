65b0926332a5516600b2548f056ddb6c3f0c0264
Nathaniel Roberts <57020731+NJRCHK@users.noreply.github.com>
2020-01-24 16:04:33 -0800

    Fixed formatting

--- VR Pacman 2019/Assets/Scripts/FruitSpawns.cs
+++ VR Pacman 2019/Assets/Scripts/FruitSpawns.cs
@@ -9,14 +9,14 @@ public class FruitSpawns : MonoBehaviour {
 	void Awake () {
 		Level = Pickup.getLevel();
 
-		if (Level == 1) { ActiveFruit = "Cherry"; }
+		if (Level == 1)     {ActiveFruit = "Cherry"; }
 		else if(Level == 2) {ActiveFruit = "Strawberry";}
 		else if(Level == 3) {ActiveFruit = "Orange";}
 		else if(Level == 4) {ActiveFruit = "Apple";}
 		else if(Level == 5) {ActiveFruit = "Melon";}
 		else if(Level == 6) {ActiveFruit = "Galaxian";}
 		else if(Level == 7) {ActiveFruit = "Bell";}
-		else if(Level > 7) {ActiveFruit = "Key";}
+		else if(Level > 7 ) {ActiveFruit = "Key";}
 
 		GameObject[] GameObjectArray = GameObject.FindGameObjectsWithTag("Fruit");
 		int i = 0; 
@@ -25,9 +25,5 @@ public class FruitSpawns : MonoBehaviour {
 			Fruit.SetActive(false);			
 			i++;
 		}
-
-
-
-
 	}
 }
