3f226eaa022403ff5f3ffef1801b3edddb7c2730
Nathaniel Roberts <57020731+NJRCHK@users.noreply.github.com>
2020-01-24 16:03:37 -0800

    Changed variables from camelcase to pascalcase

--- VR Pacman 2019/Assets/Scripts/FruitSpawns.cs
+++ VR Pacman 2019/Assets/Scripts/FruitSpawns.cs
@@ -3,28 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FruitSpawns : MonoBehaviour {
-	public string activeFruit;
+	public string ActiveFruit;
 	private static int Level;
+	GameObject ActiveFruitObject; 
 	void Awake () {
 		Level = Pickup.getLevel();
 
-		if (Level == 1) { activeFruit = "Cherry"; }
-		else if(Level == 2) {activeFruit = "Strawberry";}
-		else if(Level == 3) {activeFruit = "Orange";}
-		else if(Level == 4) {activeFruit = "Apple";}
-		else if(Level == 5) {activeFruit = "Melon";}
-		else if(Level == 6) {activeFruit = "Galaxian";}
-		else if(Level == 7) {activeFruit = "Bell";}
-		else if(Level > 7) {activeFruit = "Key";}
+		if (Level == 1) { ActiveFruit = "Cherry"; }
+		else if(Level == 2) {ActiveFruit = "Strawberry";}
+		else if(Level == 3) {ActiveFruit = "Orange";}
+		else if(Level == 4) {ActiveFruit = "Apple";}
+		else if(Level == 5) {ActiveFruit = "Melon";}
+		else if(Level == 6) {ActiveFruit = "Galaxian";}
+		else if(Level == 7) {ActiveFruit = "Bell";}
+		else if(Level > 7) {ActiveFruit = "Key";}
 
-		GameObject[] gameObjectArray = GameObject.FindGameObjectsWithTag("Fruit");
+		GameObject[] GameObjectArray = GameObject.FindGameObjectsWithTag("Fruit");
 		int i = 0; 
-		foreach (GameObject Fruit in gameObjectArray)
+		foreach (GameObject Fruit in GameObjectArray)
 		{
-			if(gameObjectArray[i].name != activeFruit)
-			{
-				Fruit.SetActive(false);
-			}
+			Fruit.SetActive(false);			
 			i++;
 		}
 
