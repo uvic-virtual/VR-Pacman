8e794bce94c2e077dce625d5a2a8bd6b7e1c2311
samsara138 <alex116.dll@gmail.com>
2020-02-14 15:51:28 -0800

    sound

--- VR Pacman 2019/Assets/Scripts/Audio/AudioManager.cs
+++ VR Pacman 2019/Assets/Scripts/Audio/AudioManager.cs
@@ -26,8 +26,9 @@ public class AudioManager : MonoBehaviour {
 
     private void Start()
     {
-        Play("creepyStartupMusic");
-        Play("creepyBackgroundMusic"); //TO FIX: starts playing at the same time as startup music
+        Debug.Log("playing");
+        Play("Creepy_Startup_Music");
+        Play("Creepy_Background_Music"); //TO FIX: starts playing at the same time as startup music
     }
     //searches for passed in clipname in sounds array and plays it
     public void Play(string clipname){
