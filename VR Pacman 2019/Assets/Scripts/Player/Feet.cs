3fb55a7c7fe7fd41987417522769a34166df0a7a
samsara138 <alex116.dll@gmail.com>
2020-02-28 15:32:09 -0800

    added feet to player and updated player prefab

---
+++ VR Pacman 2019/Assets/Scripts/Player/Feet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feet : MonoBehaviour {
+	private GameObject playerFeet;
+	private GameObject playerCam;
+	// Use this for initialization
+	void Start () {
+		playerFeet = GameObject.Find("PlayerFeet");
+		playerCam = GameObject.Find("PlayerCam");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 camPos = playerCam.transform.position;
+		Vector3 feetPos = new Vector3(camPos.x, camPos.y - 1.5f, camPos.z);
+		playerFeet.transform.position = feetPos;
+	}
+}
